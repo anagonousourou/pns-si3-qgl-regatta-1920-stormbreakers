632d65ae25462a5e39de676fc637219c32e8a7a2
DavidLebrisse <david.lebrisse@etu.univ-cotedazur.fr>
2020-01-20 20:27:05 +0100

    Merge branch 'master' of C:\TOOLS\IntelliJ\IdeaProjects\courses\QGL\pns-si3-qgl-regatta-1920-stormbreakers with conflicts.

--- src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/CockpitTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/CockpitTest.java
@@ -1,11 +1,13 @@
 package fr.unice.polytech.si3.qgl.stormbreakers;
 
+import fr.unice.polytech.si3.qgl.stormbreakers.data.actions.SailorAction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.util.List;
 
 class CockpitTest {
 
@@ -15,7 +17,7 @@ class CockpitTest {
     @BeforeEach
     void setUp() throws IOException {
         this.inputInit1 = new String(this.getClass().getResourceAsStream("/init1.json").readAllBytes());
-        this.inputInit2 = new String(this.getClass().getResourceAsStream("/init2.json").readAllBytes());
+        this.inputInit3 = new String(this.getClass().getResourceAsStream("/init3.json").readAllBytes());
         this.cockpit = new Cockpit();
     }
     
@@ -27,22 +29,9 @@ class CockpitTest {
     }
 
     @Test
-    void nextRoundTestComplex(){
-        this.cockpit.initGame(inputInit2);
-        assertTrue(this.cockpit.nextRound("{}").contains("2"));
-    }
-    @Test
-    void nextRoundTestMoreComplex(){
-        this.cockpit.initGame(inputInit2);
-        String result=this.cockpit.nextRound("{}");
-        assertTrue(result.contains("0"),"Le seul rameur à gauche rame ");
-        assertTrue(result.contains("1")||result.contains("2")||result.contains("3"),
-        "Un des rameurs à droite rame" );
-        assertFalse(result.contains("1") && result.contains("2"),
-        "Un seul des rameurs à droite rame" );
-        assertFalse(result.contains("1") && result.contains("3"),
-        "Un seul des rameurs à droite rame" );
-        assertFalse(result.contains("2") && result.contains("3"),
-        "Un seul des rameurs à droite rame" );
+    void actionsTest2() {
+        cockpit.initGame(inputInit3);
+        List<SailorAction> myActions = cockpit.actions();
+        assertEquals(2, myActions.size());
     }
 }
