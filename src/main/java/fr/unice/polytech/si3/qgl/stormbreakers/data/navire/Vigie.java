adbab8fb53a759f73a1a33a0d4116ff88e394e3c
DavidLebrisse <david.lebrisse@etu.univ-cotedazur.fr>
2020-02-01 11:35:40 +0100

    #8 Codes des equipements centralisés dans EquipmentType (et Equipment atm)

--- src/main/java/fr/unice/polytech/si3/qgl/stormbreakers/data/navire/Vigie.java
+++ src/main/java/fr/unice/polytech/si3/qgl/stormbreakers/data/navire/Vigie.java
@@ -3,7 +3,7 @@ package fr.unice.polytech.si3.qgl.stormbreakers.data.navire;
 public class Vigie extends Equipment {
 
     Vigie(int x, int y) {
-        super("watch",x,y);
+        super(EquipmentType.WATCH.code,x,y);
     }
 
 }
