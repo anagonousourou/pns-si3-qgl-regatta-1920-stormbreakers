b7651dbd19280e92873b23c9ebafbdb77156d13e
DavidLebrisse <david.lebrisse@etu.univ-cotedazur.fr>
2020-02-02 20:18:05 +0100

    #8 Test de l'ordre des checkpoints récupérés

--- src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/processing/communication/InputParserTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/processing/communication/InputParserTest.java
@@ -3,6 +3,7 @@ package fr.unice.polytech.si3.qgl.stormbreakers.processing.communication;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import fr.unice.polytech.si3.qgl.stormbreakers.data.objective.Checkpoint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,17 +13,24 @@ import fr.unice.polytech.si3.qgl.stormbreakers.data.metrics.Position;
 import fr.unice.polytech.si3.qgl.stormbreakers.data.objective.RegattaGoal;
 import fr.unice.polytech.si3.qgl.stormbreakers.data.ocean.Bateau;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class InputParserTest {
 
     private InputParser parser;
     private String initGameExample;
     private String nextRoundExample;
 
+    private String init2Checkpoints;
+
     @BeforeEach
     void SetUp() {
         parser = new InputParser();
         initGameExample = "{\"goal\": {\"mode\": \"REGATTA\",\"checkpoints\": [{\"position\": {\"x\": 1000,\"y\": 0,\"orientation\": 0},\"shape\": {\"type\": \"circle\",\"radius\": 50}}]},\"shipCount\": 1,\"ship\": {\"type\": \"ship\",\"life\": 100,\"position\": {\"x\": 0,\"y\": 0,\"orientation\": 0},\"name\": \"Les copaings d'abord!\",\"deck\": {\"width\": 2,\"length\": 1},\"entities\": [{\"x\": 0,\"y\": 0,\"type\": \"oar\"},{\"x\": 1,\"y\": 0,\"type\": \"oar\"}],\"shape\": {\"type\": \"rectangle\",\"width\": 2,\"height\": 3,\"orientation\": 0}},\"sailors\": [{\"x\": 0,\"y\": 0,\"id\": 0,\"name\": \"Edward Teach\"},{\"x\": 0,\"y\": 1,\"id\": 1,\"name\": \"Tom Pouce\"}]}";
         nextRoundExample = "{\"ship\": {\"type\": \"ship\",\"life\": 100,\"position\": {\"x\": 10.654,\"y\": 3,\"orientation\": 2.05},\"name\": \"Les copaings d'abord!\",\"deck\": {\"width\": 2,\"length\": 1},\"entities\": [{\"x\": 0,\"y\": 0,\"type\": \"oar\"},{\"x\": 1,\"y\": 0,\"type\": \"oar\"}]},\"visibleEntities\": []}";
+
+        init2Checkpoints = "{\"goal\": {\"mode\": \"REGATTA\",\"checkpoints\": [{\"position\": {\"x\": 1000,\"y\": 0,\"orientation\": 0},\"shape\": {\"type\": \"circle\",\"radius\": 50}},{\"position\": {\"x\": 0,\"y\": 0,\"orientation\": 0},\"shape\": {\"type\": \"circle\",\"radius\": 50}}]},\"shipCount\": 1,\"ship\": {\"type\": \"ship\",\"life\": 100,\"position\": {\"x\": 0,\"y\": 0,\"orientation\": 0},\"name\": \"Les copaings d'abord!\",\"deck\": {\"width\": 2,\"length\": 1},\"entities\": [{\"x\": 0,\"y\": 0,\"type\": \"oar\"},{\"x\": 1,\"y\": 0,\"type\": \"oar\"}],\"shape\": {\"type\": \"rectangle\",\"width\": 2,\"height\": 3,\"orientation\": 0}},\"sailors\": [{\"x\": 0,\"y\": 0,\"id\": 0,\"name\": \"Edward Teach\"},{\"x\": 0,\"y\": 1,\"id\": 1,\"name\": \"Tom Pouce\"}]}";
     }
 
     @Test
@@ -78,6 +86,31 @@ class InputParserTest {
 
         assertEquals(0, next.getVisibleEntities().size());
 
+
+    }
+
+    @Test
+    void testFetchInitWhen2Checkpoints() {
+        InitGame init = parser.fetchInitGameState(init2Checkpoints);
+
+        RegattaGoal goal = (RegattaGoal) init.getGoal();
+        assertAll("goal",
+                () -> assertEquals("REGATTA", init.getGoal().getMode()),
+                () -> assertEquals(2,goal.getCheckpoints().size())
+        );
+
+        List<Checkpoint> checkpoints = goal.getCheckpoints();
+        assertAll("checkpoint 1",
+                () -> assertEquals(1000,checkpoints.get(0).getPosition().getX()),
+                () -> assertEquals(0,checkpoints.get(0).getPosition().getY()),
+                () -> assertEquals(0,checkpoints.get(0).getPosition().getOrientation())
+        );
+
+        assertAll("checkpoint 2",
+                () -> assertEquals(0,checkpoints.get(1).getPosition().getX()),
+                () -> assertEquals(0,checkpoints.get(1).getPosition().getY()),
+                () -> assertEquals(0,checkpoints.get(1).getPosition().getOrientation())
+        );
 
     }
 }
