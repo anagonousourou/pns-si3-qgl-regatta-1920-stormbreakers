3d654e86a302a007d3ac0501ee1495877c91c1b6
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-02-08 21:04:31 +0100

    sauvegarde

--- src/main/java/fr/unice/polytech/si3/qgl/stormbreakers/simulation/Simulator.java
+++ src/main/java/fr/unice/polytech/si3/qgl/stormbreakers/simulation/Simulator.java
@@ -16,63 +16,86 @@ class Simulator {
     String round1;
     EquimentManager equimentManager;
     Boat boat;
-    InputParser ip=new InputParser();
+    InputParser ip = new InputParser();
+
     public static void main(String[] args) throws IOException {
 
         Simulator sim = new Simulator();
         sim.setUpSimulation();
         sim.startSimulation();
         /*
-
-        sim.launchGame(c);
-        System.out.println(sim.firstNextRound(c));*/
+         * 
+         * sim.launchGame(c); System.out.println(sim.firstNextRound(c));
+         */
 
     }
 
     Simulator() throws IOException {
-        this.init=new String(this.getClass().getResourceAsStream("/simul/init5.json").readAllBytes());
-        this.round1=new String(this.getClass().getResourceAsStream("/simul/round1.json").readAllBytes());
+        this.init = new String(this.getClass().getResourceAsStream("/simul/init5.json").readAllBytes());
+        this.round1 = new String(this.getClass().getResourceAsStream("/simul/round1.json").readAllBytes());
     }
 
     /**
-     * Fonction pour créer le Boat 'virtuel' pour executer les actions des marins 
+     * Fonction pour créer le Boat 'virtuel' pour executer les actions des marins
+     * 
      * @throws JsonMappingException
      * @throws JsonProcessingException
      * @throws IOException
      */
 
     void setUpSimulation() throws JsonMappingException, JsonProcessingException, IOException {
-        this.crew= new Crew( ip.fetchAllSailors(init));
+        this.crew = new Crew(ip.fetchAllSailors(init));
         this.equimentManager = new EquimentManager(ip.fetchAllOars(init), ip.fetchWidth(init));
-        boat=new Boat();
+        boat = new Boat();
         this.boat.setCrew(this.crew);
-        System.out.println("Crew 0:" +this.crew.toList());
+        this.boat.setEquipmentManager(this.equimentManager);
+        System.out.println("Crew 0:" + this.crew.toList());
 
     }
+
     /**
-     * fonction pour créer le cokpit lui passer les données d'initialisation et lui renvoyer les nextRound
+     * fonction pour créer le cokpit lui passer les données d'initialisation et lui
+     * renvoyer les nextRound
+     * 
      * @throws JsonMappingException
      * @throws JsonProcessingException
      */
     void startSimulation() throws JsonMappingException, JsonProcessingException {
         Cockpit c = new Cockpit();
         launchGame(c);
-        String actions= firstNextRound(c);
-        List<MoveAction> moves=ip.fetchMoves(actions);
-        List<OarAction> oarActions=ip.fetchOarActions(actions);
+        String actions = firstNextRound(c);
+        List<MoveAction> moves = ip.fetchMoves(actions);
+        List<OarAction> oarActions = ip.fetchOarActions(actions);
         System.out.println(moves);
         this.crew.executeMoves(moves);
         this.crew.executeActions(oarActions);
-        System.out.println("\n Crew 1:" +this.crew.toList());
+        System.out.println("Rotation: "+this.angleRotation());
+        //System.out.println("\n Crew 1:" + this.crew.toList());
     }
 
-    void launchGame(ICockpit ic){
+    void launchGame(ICockpit ic) {
         ic.initGame(init);
     }
 
-    String firstNextRound(ICockpit ic){
+    String firstNextRound(ICockpit ic) {
         return ic.nextRound(round1);
     }
 
-    
+    double angleRotation() {
+        double radialSpeed = 0;
+        double rotationSpeedPerOar = (Math.PI/2) / this.equimentManager.oars.size();
+        // OARS
+        for (Oar oar : this.equimentManager.oars) {
+            if (oar.isUsed()) {
+                if (this.equimentManager.rightOars.contains(oar)) {
+                    radialSpeed += rotationSpeedPerOar;
+                } else if (this.equimentManager.leftOars.contains(oar)) {
+                    radialSpeed -= rotationSpeedPerOar;
+                }
+            }
+        }
+
+        return radialSpeed;
+    }
+
 }
