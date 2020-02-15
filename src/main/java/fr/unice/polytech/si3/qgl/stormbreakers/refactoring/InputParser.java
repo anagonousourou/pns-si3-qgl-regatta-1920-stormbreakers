4df427a349a3e71c8fc9ec9e0f3fb7388f4f094a
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-02-15 22:48:08 +0100

    revue qualite encore une fois

--- src/main/java/fr/unice/polytech/si3/qgl/stormbreakers/refactoring/InputParser.java
+++ src/main/java/fr/unice/polytech/si3/qgl/stormbreakers/refactoring/InputParser.java
@@ -24,29 +24,29 @@ import fr.unice.polytech.si3.qgl.stormbreakers.data.objective.Checkpoint;
 
 public class InputParser {
 	ObjectMapper mapper = new ObjectMapper();
-	private static final String shapeKey="shape";
-	private static final String idKey="id"; 
-	private static final String shipKey="ship";
-	private static final String entitiesKey="entities";
-	private static final String checkpointsKey="checkpoints";
-	private static final String sailorsKey="sailors";
-	private static final String xKey="x";
-	private static final String yKey="y";
-	private static final String orientationKey="orientation";
-	private static final String positionKey="position";
+	private static final String SHAPE_KEY="shape";
+	private static final String ID_KEY="id"; 
+	private static final String SHIP_KEY="ship";
+	private static final String ENTITIES_KEY="entities";
+	private static final String CHECKPOINTS_KEY="checkpoints";
+	private static final String SAILORS_KEY="sailors";
+	private static final String XKEY="x";
+	private static final String YKEY="y";
+	private static final String ORIENTATION_KEY="orientation";
+	private static final String POSITION_KEY="position";
 
 	public List<Marine> fetchAllSailors(String jsonInput) throws JsonProcessingException {
 		List<Marine> marins = new ArrayList<>();
-		mapper.readTree(jsonInput).get(sailorsKey).forEach(s -> 
-			marins.add(new Marine(s.get(idKey).asInt(), s.get(xKey).asInt(), s.get(yKey).asInt()))
+		mapper.readTree(jsonInput).get(SAILORS_KEY).forEach(s -> 
+			marins.add(new Marine(s.get(ID_KEY).asInt(), s.get(XKEY).asInt(), s.get(YKEY).asInt()))
 		);
 		return marins;
 	}
 
 	public List<Oar> fetchAllOars(String jString) throws JsonProcessingException {
 		List<Oar> oars = new ArrayList<>();
-		mapper.readTree(jString).get(shipKey).get(entitiesKey).forEach(s -> 
-			oars.add(new Oar(s.get(xKey).asInt(), s.get(yKey).asInt()))
+		mapper.readTree(jString).get(SHIP_KEY).get(ENTITIES_KEY).forEach(s -> 
+			oars.add(new Oar(s.get(XKEY).asInt(), s.get(YKEY).asInt()))
 		);
 
 		return oars;
@@ -64,23 +64,23 @@ public class InputParser {
 	 */
 	public List<Checkpoint> fetchCheckpoints(String jString) throws JsonProcessingException {
 		List<Checkpoint> checkpoints = new ArrayList<>();
-		mapper.readTree(jString).get("goal").get(checkpointsKey).forEach(c -> {
+		mapper.readTree(jString).get("goal").get(CHECKPOINTS_KEY).forEach(c -> {
 			Shape shape;
-			if (c.get(shapeKey).get("type").asText().equals("circle")) {
-				shape = new Circle(c.get(shapeKey).get("radius").asDouble());
+			if (c.get(SHAPE_KEY).get("type").asText().equals("circle")) {
+				shape = new Circle(c.get(SHAPE_KEY).get("radius").asDouble());
 			} else {
-				shape = new Rectangle(c.get(shapeKey).get("width").asDouble(), c.get(shapeKey).get("height").asDouble(),
-						c.get(shapeKey).get(orientationKey).asDouble());
+				shape = new Rectangle(c.get(SHAPE_KEY).get("width").asDouble(), c.get(SHAPE_KEY).get("height").asDouble(),
+						c.get(SHAPE_KEY).get(ORIENTATION_KEY).asDouble());
 			}
 			checkpoints.add(new Checkpoint(
-					new Position(c.get(positionKey).get("x").asInt(), c.get(positionKey).get("y").asInt()), shape));
+					new Position(c.get(POSITION_KEY).get("x").asInt(), c.get(POSITION_KEY).get("y").asInt()), shape));
 		});
 		return checkpoints;
 	}
 
 	public List<Equipment> fetchEquipments(String jString) throws JsonProcessingException {
 		List<Equipment> equipments = new ArrayList<>();
-		mapper.readTree(jString).get(shipKey).get("entities").forEach(e -> {
+		mapper.readTree(jString).get(SHIP_KEY).get(ENTITIES_KEY).forEach(e -> {
 			Equipment equipment = null;
 			if (e.get("type").asText().equals("oar")) {
 				equipment = new Oar(e.get("x").asInt(), e.get("y").asInt());
@@ -98,23 +98,23 @@ public class InputParser {
 	}
 
 	public Position fetchBoatPosition(String jString) throws JsonProcessingException{
-		JsonNode result = mapper.readTree(jString).get(shipKey);
-		return new Position(result.get(positionKey).get(xKey).asDouble(),
-		result.get(positionKey).get(yKey).asDouble(), result.get(positionKey).get(orientationKey).asDouble());
+		JsonNode result = mapper.readTree(jString).get(SHIP_KEY);
+		return new Position(result.get(POSITION_KEY).get(XKEY).asDouble(),
+		result.get(POSITION_KEY).get(YKEY).asDouble(), result.get(POSITION_KEY).get(ORIENTATION_KEY).asDouble());
 	}
 
 	public int fetchBoatLength(String jString) throws JsonProcessingException{
-		return mapper.readTree(jString).get(shipKey).get("deck").get("length").asInt();
+		return mapper.readTree(jString).get(SHIP_KEY).get("deck").get("length").asInt();
 	}
 	
 
 	public int fetchBoatLife(String jString) throws JsonProcessingException{
-		JsonNode result = mapper.readTree(jString).get(shipKey);
+		JsonNode result = mapper.readTree(jString).get(SHIP_KEY);
 		return result.get("life").asInt();
 	}
 
 	public int fetchBoatWidth(String jString) throws JsonProcessingException {
-		return mapper.readTree(jString).get(shipKey).get("deck").get("width").asInt();
+		return mapper.readTree(jString).get(SHIP_KEY).get("deck").get("width").asInt();
 	}
 
 
@@ -132,7 +132,7 @@ public class InputParser {
 		if (result == null) {
 			return 0.0;
 		} else {
-			return  result.get(orientationKey).asDouble();
+			return  result.get(ORIENTATION_KEY).asDouble();
 		}
 	}
 
