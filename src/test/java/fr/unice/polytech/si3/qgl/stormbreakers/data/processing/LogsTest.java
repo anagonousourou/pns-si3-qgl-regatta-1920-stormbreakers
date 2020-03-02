bb81b29ec56a0b53327b0f98c72d91a5e6a52d05
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-03-02 18:24:57 +0100

    ajout de tests

--- src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/data/processing/LogsTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/data/processing/LogsTest.java
@@ -10,11 +10,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import fr.unice.polytech.si3.qgl.stormbreakers.data.processing.Logger;
-
-import java.io.IOException;
-import java.util.List;
-
 class LogsTest {
 
     private Logger logger = Logger.getInstance();
