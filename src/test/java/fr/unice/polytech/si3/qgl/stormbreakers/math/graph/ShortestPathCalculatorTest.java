c9ca974bcca600eb24cdf537eef5389b0f8be115
hebabz <heba.bouzidi@etu.unice.fr>
2020-03-16 13:29:15 +0100

    Tests if edges are distincts + if last edge is the position of the cp #69

--- src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/math/graph/ShortestPathCalculatorTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/math/graph/ShortestPathCalculatorTest.java
@@ -1,6 +1,8 @@
 package fr.unice.polytech.si3.qgl.stormbreakers.math.graph;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,19 +11,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import fr.unice.polytech.si3.qgl.stormbreakers.data.metrics.Circle;
-import fr.unice.polytech.si3.qgl.stormbreakers.data.metrics.IPoint;
 import fr.unice.polytech.si3.qgl.stormbreakers.data.metrics.Position;
 import fr.unice.polytech.si3.qgl.stormbreakers.data.metrics.Rectangle;
 import fr.unice.polytech.si3.qgl.stormbreakers.data.objective.Checkpoint;
 import fr.unice.polytech.si3.qgl.stormbreakers.data.ocean.Recif;
-import fr.unice.polytech.si3.qgl.stormbreakers.math.Point2D;
 
 public class ShortestPathCalculatorTest {
 	//ListeDAdjacence adj;
 	Sommet bateau;
 	List<Recif> recifs;
 	Recif r1, r2, r3, r4;
-	Checkpoint cp, cp2, cp3, cp4, cp5, cpRemoveUseless;
+	Checkpoint cp, cp2, cp3, cp4, cp5, cp6, cpRemoveUseless;
 	ShortestPathCalculator spc;
 	
 	@BeforeEach  
@@ -38,7 +38,7 @@ public class ShortestPathCalculatorTest {
 		cp3 = new Checkpoint(new Position(1200, 1300), new Circle(50));
 		cp4 = new Checkpoint(new Position(1700, 1700), new Circle(50));
 		cp5 = new Checkpoint(new Position(1250, 2600), new Circle(50));
-		cp5 = new Checkpoint(new Position(1250, 2600), new Circle(50));
+		cp6 = new Checkpoint(new Position(2800, 1300), new Circle(50));
 		cpRemoveUseless= new Checkpoint(new Position(1750.5, 1750.5), new Circle(50));
 		spc= new ShortestPathCalculator();
 		//adj= new ListeDAdjacence(bateau, other,recifs, cp);
@@ -46,33 +46,57 @@ public class ShortestPathCalculatorTest {
 	
 	@Test
 	public void shortestPathFromBoatPosTest() {
-		spc.shortestPathFromBoatPos(bateau, recifs,cp);
-		assertTrue(true);
-		spc.shortestPathFromBoatPos(bateau, recifs,cp2);
-		assertTrue(true);
+		List<Sommet> edges1 = spc.shortestPathFromBoatPos(bateau, recifs,cp);
+		assertEquals(edges1.size(), edges1.stream().distinct().count());
+		assertEquals(0, edges1.get(edges1.size()-1).getPoint().distanceTo(cp.getPosition()));
+		
+		List<Sommet> edges2 = spc.shortestPathFromBoatPos(bateau, recifs,cp2);
+		assertEquals(edges2.size(), edges2.stream().distinct().count());
+		assertEquals(0, edges2.get(edges2.size()-1).getPoint().distanceTo(cp2.getPosition()));
 	}
 	
 	@Test
 	public void shortestPathFromBoatPosWithReevesTest() {
-		assertTrue(true);
-		recifs= new ArrayList<Recif>(List.of(r1, r2));
-		spc.shortestPathFromBoatPos(bateau, recifs, cp3);
-		assertTrue(true);
-		spc.shortestPathFromBoatPos(bateau, recifs, cp4);
-		assertTrue(true);
-		spc.shortestPathFromBoatPos(bateau, recifs, cp5);
-		assertTrue(true);
+		recifs = new ArrayList<Recif>(List.of(r1, r2));
+		List<Sommet> edges1 = spc.shortestPathFromBoatPos(bateau, recifs, cp3);
+		assertEquals(edges1.size(), edges1.stream().distinct().count());
+		assertEquals(0, edges1.get(edges1.size()-1).getPoint().distanceTo(cp3.getPosition()));
+		
+		List<Sommet> edges2 = spc.shortestPathFromBoatPos(bateau, recifs, cp4);
+		assertEquals(edges2.size(), edges2.stream().distinct().count());
+		assertEquals(0, edges2.get(edges2.size()-1).getPoint().distanceTo(cp4.getPosition()));
+		
+		List<Sommet> edges3 = spc.shortestPathFromBoatPos(bateau, recifs, cp5);
+		assertEquals(edges3.size(), edges3.stream().distinct().count());
+		assertEquals(0, edges3.get(edges3.size()-1).getPoint().distanceTo(cp5.getPosition()));
+		
 	}
 	@Test
 	public void shortestPathFromBoatPosWithManyReevesTest() {
-		recifs= new ArrayList<Recif>(List.of(r1, r2, r3, r4));
-		assertTrue(true);
-		spc.shortestPathFromBoatPos(bateau, recifs, cp);
-		spc.shortestPathFromBoatPos(bateau, recifs, cp2);
-		spc.shortestPathFromBoatPos(bateau, recifs, cp3);
-		spc.shortestPathFromBoatPos(bateau, recifs, cp4);
-		spc.shortestPathFromBoatPos(bateau, recifs, cp5);
-		assertTrue(true);
+		recifs = new ArrayList<Recif>(List.of(r1, r2, r3, r4));
+		List<Sommet> edges1 = spc.shortestPathFromBoatPos(bateau, recifs, cp);
+		assertEquals(edges1.size(), edges1.stream().distinct().count());
+		assertEquals(0, edges1.get(edges1.size()-1).getPoint().distanceTo(cp.getPosition()));
+		
+		List<Sommet> edges2 = spc.shortestPathFromBoatPos(bateau, recifs, cp2);
+		assertEquals(edges2.size(), edges2.stream().distinct().count());
+		assertEquals(0, edges2.get(edges2.size()-1).getPoint().distanceTo(cp2.getPosition()));
+
+		List<Sommet> edges3 = spc.shortestPathFromBoatPos(bateau, recifs, cp3);
+		assertEquals(edges3.size(), edges3.stream().distinct().count());
+		assertEquals(0, edges3.get(edges3.size()-1).getPoint().distanceTo(cp3.getPosition()));
+		
+		List<Sommet> edges4 = spc.shortestPathFromBoatPos(bateau, recifs, cp4);
+		assertEquals(edges4.size(), edges4.stream().distinct().count());
+		assertEquals(0, edges4.get(edges4.size()-1).getPoint().distanceTo(cp4.getPosition()));
+		
+		List<Sommet> edges5 = spc.shortestPathFromBoatPos(bateau, recifs, cp5);
+		assertEquals(edges5.size(), edges5.stream().distinct().count());
+		assertEquals(0, edges5.get(edges5.size()-1).getPoint().distanceTo(cp5.getPosition()));
+		
+		List<Sommet> edges6 = spc.shortestPathFromBoatPos(bateau, recifs, cp6);
+		assertEquals(edges6.size(), edges6.stream().distinct().count());
+		assertEquals(0, edges6.get(edges6.size()-1).getPoint().distanceTo(cp6.getPosition()));
 	}
 	
 	@Test 
