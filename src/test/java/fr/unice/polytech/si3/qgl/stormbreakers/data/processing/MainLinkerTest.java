15a9ffc0f84b6f68cb97f0efc3a0c74ca8df3312
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-05-01 14:37:37 +0200

    add tests for week9

--- src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/data/processing/MainLinkerTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/stormbreakers/data/processing/MainLinkerTest.java
@@ -13,6 +13,8 @@ public class MainLinkerTest {
     private String game;
     private String gameArchipel;
     private String gameHarbour;
+    private String gameSnake;
+    private String roundSnake;
     private String roundHarbour;
     private String roundHarbour2;
     private String round1;
@@ -20,6 +22,7 @@ public class MainLinkerTest {
 
     @BeforeEach
     public void setUp() throws IOException {
+        gameSnake=new String(this.getClass().getResourceAsStream("/elementstest/init_snake.json").readAllBytes());
         gameHarbour = new String(this.getClass().getResourceAsStream("/elementstest/init_harbour.json").readAllBytes());
         game = new String(this.getClass().getResourceAsStream("/elementstest/init.json").readAllBytes());
         gameArchipel = new String(
@@ -32,6 +35,9 @@ public class MainLinkerTest {
                 this.getClass().getResourceAsStream("/elementstest/round_harbour.json").readAllBytes());
                 roundHarbour2 = new String(
                 this.getClass().getResourceAsStream("/elementstest/round_harbour2.json").readAllBytes());
+        roundSnake=new String(
+            this.getClass().getResourceAsStream("/elementstest/round_snake.json").readAllBytes()
+        );
     }
 
     @Test
@@ -54,6 +60,12 @@ public class MainLinkerTest {
         assertDoesNotThrow(() -> System.out.println(constructor.sendActions(roundHarbour2)));
     }
 
+
+    @Test
+    public void snakeTest(){
+        constructor=new MainLinker(gameSnake);
+        assertDoesNotThrow(()->System.out.println(constructor.sendActions(roundSnake)));
+    }
 
 
 }
